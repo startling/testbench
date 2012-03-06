10ef1ff0c537b548bebd333c2c4013ffc47882bf
tim <tdixon51793@gmail.com>
2012-03-06 17:38:11 -0600

    base .gitignore, setup.py, and __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = "testbench",
+    version = "0.00.0dev",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    description = "a benchmarking library in the vein of unittest",
+    packages = ["testbench"],
+    scripts = [],
+)
