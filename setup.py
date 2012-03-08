ecc66708c35a0d751726fde1f8c469ee89f7d9f4
tim <tdixon51793@gmail.com>
2012-03-08 00:52:52 -0600

    bump the version string. first pre-release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "testbench",
-    version = "0.00.0dev",
+    version = "0.01.0dev",
     author = "startling",
     author_email = "tdixon51793@gmail.com",
     description = "a benchmarking library in the vein of unittest",
