f7bd65f1e55c36ccd6289bce9a20d9660195a351
tim <tdixon51793@gmail.com>
2012-03-07 23:07:15 -0600

    moved `average` to `testbench.utilities`

---
+++ testbench/utilities.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def average(iterable):
+    "A generic `average` function that works on any iterable, including iterators."
+    total = float(0)
+    count = float(0)
+    for n in iterable:
+        total += n
+        count += 1
+    return total/count
